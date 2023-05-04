9f55b912705f0f7c55dd1e79cdccfd3af37c446a
Valeriya Subocheva <96348645+SubochevaValeriya@users.noreply.github.com>
2023-05-04 22:13:39 +0300

    Made function for executing a command

--- internal/app/watch-and-run/performer.go
+++ internal/app/watch-and-run/performer.go
@@ -1,5 +1,40 @@
-package wathAndRun //?
+package worker
+
+import (
+	"os"
+	"os/exec"
+)
 
 type Executer interface { //naming
 	ExecuteCommand()
+}
+
+func executeCommand(command string, logFile string) error {
+	cmd := exec.Command(command)
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		return err
+	}
+
+	err = writeToFile(stdout, logFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeToFile(data []byte, logfile string) error {
+	f, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	if _, err := f.Write(data); err != nil {
+		return err
+	}
+	if err := f.Close(); err != nil {
+		return err
+	}
+	return nil
 }
